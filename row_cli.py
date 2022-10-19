aa7483aa40188d7d3a959beb5df7cd1a64cfae7d
diamonddev107 <kisney.dev.11@gmail.com>
2022-10-19 17:08:14 -0600

    fix: correct docopt syntax

--- row_cli.py
+++ row_cli.py
@@ -2,21 +2,25 @@
 # * coding: utf8 *
 """
 UDOT Right of Way (ROW) Parcel Number Extraction
+
 Usage:
-    row storage download (--from-bucket=bucket --task-index=index) [--testing=test]
-    row images process <file_name>
-    row image convert <file_name>
-    row image rotate <file_name>
-    row circle prepare <file_name>
-    row circle detect <prepared_file_name>
-    row circle crop <file_name>
-    row ocr prepare <file_name>
-    row ocr detect <image>
-    row results write
+    row_cli.py storage download (--from-bucket=bucket --task-index=index) [--testing=test]
+    row_cli.py images process <file_name>
+    row_cli.py image convert <file_name> (--output-directory=directory)
+    row_cli.py image rotate <file_name>
+    row_cli.py circle prepare <file_name>
+    row_cli.py circle detect <prepared_file_name>
+    row_cli.py circle crop <file_name>
+    row_cli.py ocr prepare <file_name>
+    row_cli.py ocr detect <image>
+    row_cli.py results write
+
 Options:
-    --from-bucket=bucket    The bucket to find the image
-    --testing=test          Trick the tool to not use google data and from and to become file paths [default: false]
-    --task-index=index      The index of the task running
+    --from-bucket=bucket            The bucket to find the image
+    --testing=test                  Trick the tool to not use google data and from and to become
+                                        file paths [default: false]
+    --task-index=index              The index of the task running
+    --output-directory=directory    The location to output the stuff
 Examples:
     row storage download --from-bucket=bucket-name --task-index=0
     row
