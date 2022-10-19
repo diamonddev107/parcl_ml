8cb89c3198276e74dd94a32d014ba2fc260e0b33
diamonddev107 <kisney.dev.11@gmail.com>
2022-10-19 15:54:46 -0600

    feat: add ability to convert pdf's to pils

--- row.py
+++ row.py
@@ -4,6 +4,8 @@
 UDOT Right of Way (ROW) Parcel Number Extraction
 Right of way module containing methods
 """
+from pdf2image import convert_from_bytes
+from pdf2image.exceptions import PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError
 
 
 def main():
@@ -15,3 +17,27 @@ def function():
     """doc string"""
 
     return "hi"
+
+
+def convert_pdf_to_pil(pdf_as_bytes):
+    """convert pdf to jpg images
+
+    pdf_as_bytes: a pdf as bytes
+
+    returns: a list of images and the count of images
+    """
+    dpi = 300
+    images = []
+    messages = ""
+
+    try:
+        images = convert_from_bytes(pdf_as_bytes, dpi)
+    except (TypeError, PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError) as error:
+        print(error)
+        messages = error
+
+    count = len(images)
+
+    print(f"number of images: {count}")
+
+    return (images, count, messages)
