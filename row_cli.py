ac779165f25c10f6c254bca5b6825d7f249f53c0
diamonddev107 <kisney.dev.11@gmail.com>
2022-12-07 21:37:20 -0700

    feat: add function to get bucket file names

--- row_cli.py
+++ row_cli.py
@@ -4,7 +4,7 @@
 UDOT Right of Way (ROW) Parcel Number Extraction
 
 Usage:
-    row_cli.py storage download (--from-bucket=bucket --task-index=index) [--testing=test]
+    row_cli.py storage get_job_files (--from-bucket=bucket --task-index=index) [--job-size=size --testing=test]
     row_cli.py images process <file_name>
     row_cli.py image convert <file_name> (--output-directory=directory)
     row_cli.py image rotate <file_name>
@@ -18,8 +18,9 @@ Usage:
 Options:
     --from-bucket=bucket            The bucket to find the image
     --testing=test                  Trick the tool to not use google data and from and to become
-                                        file paths [default: false]
+                                        file paths [default: False]
     --task-index=index              The index of the task running
+    --job-size=size                 The size of the job [default: 10]
     --output-directory=directory    The location to output the stuff
 Examples:
     row_cli.py storage download --from-bucket=bucket-name --task-index=0
@@ -37,13 +38,10 @@ def main():
     """doc string"""
     args = docopt(__doc__, version="1.0")  # type: ignore
 
-    if args["test"]:
-        print("great job dude")
+    if args["storage"] and args["get_job_files"]:
+        jobs = row.get_job_files(args["--from-bucket"], args["--task-index"], args["--job-size"], args["--testing"])
+        print(jobs)
         return
-
-    if args["storage"] and args["download"]:
-        return
-        # row.download(args["--from-bucket"], args["--task-index"], args["--testing"])
 
     if args["images"] and args["process"]:
         return
