b541e1c1c70bfea4aece52e5d6b564d4b4d5c5d6
diamonddev107 <kisney.dev.11@gmail.com>
2022-12-07 21:38:39 -0700

    test: add tests for get_job_files

--- row_test.py
+++ row_test.py
@@ -6,6 +6,7 @@ A module that contains tests for the project module.
 """
 
 from pathlib import Path
+from unittest.mock import Mock, PropertyMock, patch
 
 import row
 
@@ -49,3 +50,56 @@ def test_convert_pdf_to_pil_handles_empty_bytes():
     assert count == 0
     assert images == []
     assert message != ""
+
+
+@patch("google.cloud.storage.Client")
+def test_get_job_files_returns_the_page_size(storage_client):
+    job_size = 10
+    page_index = 0
+
+    page = iter([Mock() for _ in range(job_size)])
+
+    iterator = Mock()
+    iterator.pages = iter([page])
+    type(iterator).page_number = PropertyMock(return_value=1)
+
+    client = Mock()
+    client.list_blobs = Mock(return_value=iterator)
+
+    storage_client.return_value = client
+
+    jobs = row.get_job_files("bucket", page_index, job_size)
+
+    assert jobs is not None
+    assert len(jobs) == 10
+
+
+@patch("google.cloud.storage.Client")
+def test_get_job_files_returns_the_page_size_for_the_second_page(storage_client):
+    job_size = 10
+    page_index = 2
+
+    blob = Mock()
+    type(blob).name = PropertyMock(return_value="page2")
+
+    page_one_blob = Mock()
+
+    page1 = iter([page_one_blob for _ in range(job_size)])
+    page2 = iter([blob for _ in range(job_size)])
+
+    iterator = Mock()
+    iterator.pages = iter([page1, page2])
+    #: logging calls invoke the side effect
+    type(iterator).page_number = PropertyMock(side_effect=[1, 1, 1, 2, 2, 2])
+
+    client = Mock()
+    client.list_blobs = Mock(return_value=iterator)
+
+    storage_client.return_value = client
+
+    jobs = row.get_job_files("bucket", page_index, job_size)
+
+    assert jobs is not None
+    assert len(jobs) == 10
+    assert jobs[0] == "page2"
+    page_one_blob.assert_not_called()
