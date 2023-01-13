d00eac301f23eedf79a7811d3072fd3f2e130358
diamonddev107 <kisney.dev.11@gmail.com>
2023-01-13 16:22:15 -0700

    refactor: wrap row_run in main function

--- row_run.py
+++ row_run.py
@@ -15,6 +15,7 @@ import pandas as pd
 
 import row
 
+#: Set up logging
 logging.basicConfig(
     stream=stdout,
     format="%(levelname)-7s %(asctime)s %(module)10s:%(lineno)5s %(message)s",
@@ -22,61 +23,62 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
-start_seconds = perf_counter()
-
+#: Set up variables
 URI_TEXTFILE = Path(__file__).parent / "bucket-info" / "udot_bucket_uris.txt"
 UDOT_BUCKET_NAME = "ut-udot-row-county-parcels"
 OUTPUT_BUCKET_NAME = "ut-dts-agrc-udot-parcel-recog"
 TASK_INDEX = environ["CLOUD_RUN_TASK_INDEX"]
 TASK_COUNT = environ["CLOUD_RUN_TASK_COUNT"]
 
-storage_client = google.cloud.storage.Client()
-bucket = storage_client.bucket(UDOT_BUCKET_NAME)
+#: Set up main function
+def main():
+    start_seconds = perf_counter()
 
-#: Set up results dataframe
-df = pd.DataFrame({"Filename": [], "Parcels": []})
-df = df.astype({"Filename": object, "Parcels": object})
+    #: Set up results dataframe
+    df = pd.DataFrame({"Filename": [], "Parcels": []})
+    df = df.astype({"Filename": object, "Parcels": object})
 
-row.main()
+    #: Get files to process for this job
+    uri_list = row.get_job_files_from_text_index(UDOT_BUCKET_NAME, TASK_INDEX, TASK_COUNT, testing=False)
 
-# Should get_job_files use list_blobs or read the index from our project?
-#: Read index of URIs in the UDOT bucket (these are full URIs 'gs://bucket-name/filename.ext')
-uri_df = pd.read_csv(URI_TEXTFILE, sep="\n", skip_blank_lines=False)
-total_files = len(uri_df.index)
+    #: Initialize GCP storage client and bucket
+    storage_client = google.cloud.storage.Client()
+    bucket = bucket = storage_client.bucket(UDOT_BUCKET_NAME)
 
-#: Get files to process for this job
-uri_list = row.get_job_files_from_text_index(UDOT_BUCKET_NAME, TASK_INDEX, TASK_COUNT, testing=False)
+    #: Iterate over objects to detect circles and perform OCR
+    for uri in uri_list:
+        object_name = uri.removeprefix(f"gs://{UDOT_BUCKET_NAME}/")
+        if Path(object_name).suffix.casefold() == ".pdf":
+            images, count, messages = row.convert_pdf_to_pil(bucket.blob(object_name).download_as_bytes())
+            logging.info("%s contained %i pages and converted with message %s", object_name, count, messages)
 
-#: Iterate over objects to detect circles and perform OCR
-for uri in uri_list:
-    object_name = uri.removeprefix(f"gs://{UDOT_BUCKET_NAME}/")
-    if Path(object_name).suffix.casefold() == ".pdf":
-        images, count, messages = row.convert_pdf_to_pil(bucket.blob(object_name).download_as_bytes())
-        logging.info("%s contained %i pages and converted with message %s", object_name, count, messages)
+        elif Path(object_name).suffix.casefold() in ["jpg", "jpeg", "tif", "tiff", "png"]:
+            images = list(bucket.blob(object_name).download_as_bytes())
 
-    elif Path(object_name).suffix.casefold() in ["jpg", "jpeg", "tif", "tiff", "png"]:
-        images = list(bucket.blob(object_name).download_as_bytes())
+        else:
+            logging.info('Not a valid document or image: "%s"', object_name)
 
-    else:
-        logging.info('Not a valid document or image: "%s"', object_name)
+        #: Process images to get detected circles
+        logging.info("detecting circles in %s", object_name)
+        all_detected_circles = []
+        for image in images:
+            circle_images = row.get_circles_from_image_bytes(image)
+            all_detected_circles.extend(circle_images)  #: extend because circle_images will be a list
 
-    #: Process images to get detected circles
-    logging.info("detecting circles in %s", object_name)
-    all_detected_circles = []
-    for image in images:
-        circle_images = row.get_circles_from_image_bytes(image)
-        all_detected_circles.extend(circle_images)  #: extend because circle_images will be a list
+        #: Process detected circle images to get detected characters
+        logging.info("detecting characters in %s", object_name)
+        all_results = []
+        for circle in all_detected_circles:
+            result_str = row.get_characters_from_image(circle)
+            all_results.append(result_str)  #: append because result_str will be a string
 
-    #: Process detected circle images to get detected characters
-    logging.info("detecting characters in %s", object_name)
-    all_results = []
-    for circle in all_detected_circles:
-        result_str = row.get_characters_from_image(circle)
-        all_results.append(result_str)  #: append because result_str will be a string
+        df = row.write_results(df, object_name, all_results)
 
-    df = row.write_results(df, object_name, all_results)
+    #: Upload results to GCP bucket as CSV file
+    row.upload_csv(df, OUTPUT_BUCKET_NAME, f"ocr_results_{TASK_INDEX}.csv")
 
-#: Upload results to GCP bucket as CSV file
-row.upload_csv(df, OUTPUT_BUCKET_NAME, f"ocr_results_{TASK_INDEX}.csv")
+    logging.info("time taken for task %i: %s", TASK_INDEX, row.format_time(perf_counter() - start_seconds))
 
-logging.info("time taken for task %i: %s", TASK_INDEX, row.format_time(perf_counter() - start_seconds))
+
+if __name__ == "__main__":
+    main()
