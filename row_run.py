b65f818c8f1556045ae228152dae11aa28836580
diamonddev107 <kisney.dev.11@gmail.com>
2023-01-30 15:58:34 -0700

    chore: add more perf logs

--- row_run.py
+++ row_run.py
@@ -33,8 +33,8 @@ TASK_COUNT = environ["CLOUD_RUN_TASK_COUNT"]
 
 #: Set up main function
 def main():
-    start_seconds = perf_counter()
     """the main function to execute when cloud run starts the job"""
+    job_start = perf_counter()
 
     #: Set up results dataframe
     df = pd.DataFrame({"Filename": [], "Parcels": []})
@@ -48,38 +48,70 @@ def main():
     bucket = bucket = storage_client.bucket(UDOT_BUCKET_NAME)
 
     #: Iterate over objects to detect circles and perform OCR
-    for uri in uri_list:
-        object_name = uri.removeprefix(f"gs://{UDOT_BUCKET_NAME}/")
-        if Path(object_name).suffix.casefold() == ".pdf":
+    for object_name in files:
+        object_start = perf_counter()
+        extension = Path(object_name).suffix.casefold()
+
+        if extension == ".pdf":
+            conversion_start = perf_counter()
             images, count, messages = row.convert_pdf_to_pil(bucket.blob(object_name).download_as_bytes())
-            logging.info("%s contained %i pages and converted with message %s", object_name, count, messages)
+            logging.debug("%s contained %i pages and converted with message %s", object_name, count, messages)
+            logging.info(
+                "job %i: conversion time taken for object %s: %s",
+                TASK_INDEX,
+                object_name,
+                row.format_time(perf_counter() - conversion_start),
+            )
 
-        elif Path(object_name).suffix.casefold() in ["jpg", "jpeg", "tif", "tiff", "png"]:
+        elif extension in ["jpg", "jpeg", "tif", "tiff", "png"]:
             images = list(bucket.blob(object_name).download_as_bytes())
-
         else:
             logging.info('Not a valid document or image: "%s"', object_name)
+
+            continue
 
         #: Process images to get detected circles
         logging.info("detecting circles in %s", object_name)
         all_detected_circles = []
+        circle_start = perf_counter()
+
         for image in images:
             circle_images = row.get_circles_from_image_bytes(image)
             all_detected_circles.extend(circle_images)  #: extend because circle_images will be a list
 
+        logging.info(
+            "job %i: circle detection time taken %s: %s",
+            TASK_INDEX,
+            object_name,
+            row.format_time(perf_counter() - circle_start),
+        )
+
         #: Process detected circle images to get detected characters
         logging.info("detecting characters in %s", object_name)
         all_results = []
+        character_start = perf_counter()
+
         for circle in all_detected_circles:
             result_str = row.get_characters_from_image(circle)
             all_results.append(result_str)  #: append because result_str will be a string
 
-        df = row.write_results(df, object_name, all_results)
+        logging.info(
+            "job %i: character detection time taken %s: %s",
+            TASK_INDEX,
+            object_name,
+            row.format_time(perf_counter() - character_start),
+        )
+
+        logging.info(
+            "job %i: total time taken for entire task %s", TASK_INDEX, row.format_time(perf_counter() - object_start)
+        )
+
+        result_dataframe = row.write_results(result_dataframe, object_name, all_results)
 
     #: Upload results to GCP bucket as CSV file
     row.upload_csv(df, OUTPUT_BUCKET_NAME, f"ocr_results_{TASK_INDEX}.csv")
 
-    logging.info("time taken for task %i: %s", TASK_INDEX, row.format_time(perf_counter() - start_seconds))
+    logging.info("job %i: time taken for entire job %s", TASK_INDEX, row.format_time(perf_counter() - job_start))
 
 
 if __name__ == "__main__":
